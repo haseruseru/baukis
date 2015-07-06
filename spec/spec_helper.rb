bd5537ad7ed779727c4ee1ee1b4fbae478d16145
haseruseru <vitamin.vitamin.cwater@gmail.com>
2015-07-06 03:41:42 +0000

    add rspec_config and add test spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,7 +14,21 @@
 # users commonly want.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+  ENV["RAILS_ENV"] ||= 'test'
+  require File.expand_path("../../config/environment", __FILE__)
+  require 'rspec/rails'
+  
+  Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f}
+  
+ActiveRecord::Migration.maintain_test_schema!
+
 RSpec.configure do |config|
+  
+  config.fixture_path= "#{::Rails.root}/spec\fixtures"
+  config.use_transactional_fixtures =true
+  config.order ="random"
+  
+  
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
 =begin
