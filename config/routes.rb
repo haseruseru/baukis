3432639165457414dc96ceb284a828b8acd1c0d1
haseruseru <vitamin.vitamin.cwater@gmail.com>
2015-07-11 23:40:39 +0000

    finished chapter 10-2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   
-  namespace :staff do
+  namespace :staff ,path: '' do
     root 'top#index'
     get 'login' => 'sessions#new' , as: :login
     post 'session' => 'sessions#create',as: :session
@@ -12,7 +12,19 @@ Rails.application.routes.draw do
     get 'login' => 'sessions#new' , as: :login
     post 'session' => 'sessions#create', as: :session
     delete 'session' => 'sessions#destroy'
-  end
+    
+    resources :staff_members 
+    #つまりこういうこと
+    #get 'staff_members' => 'staff_members#index'
+    #get 'staff_members/:id' => 'staff_members#show'
+    #get 'staff_members/new' => 'staff_members#new'
+    #get 'staff_members/:id/edit' => 'staff_members#edit'
+    #post 'staff_members' => 'staff_members#create'
+    #patch 'staff_members/:id' =>'staff_members#update'
+    #delete 'staff_members/:id' => 'staff_members#destroy'
+    
+    
+    end
   
   namespace :customer do
     root 'top#index'
